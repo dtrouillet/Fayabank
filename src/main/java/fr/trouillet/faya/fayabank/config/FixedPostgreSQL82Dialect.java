5e8ad4596af0638a7c2a4d6fc4d971ebd2a8a15b
Damien Trouillet <contact@damien-trouillet.fr>
2017-08-05 01:10:07 +0200

    Add FixedPostgreSQL82Dialect

---
+++ src/main/java/fr/trouillet/faya/fayabank/config/FixedPostgreSQL82Dialect.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2017 dtrouillet
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+package fr.trouillet.faya.fayabank.config;
+
+import org.hibernate.dialect.PostgreSQL82Dialect;
+import org.hibernate.type.descriptor.sql.BinaryTypeDescriptor;
+import org.hibernate.type.descriptor.sql.SqlTypeDescriptor;
+
+import java.sql.Types;
+
+public class FixedPostgreSQL82Dialect extends PostgreSQL82Dialect {
+
+    public FixedPostgreSQL82Dialect() {
+        super();
+        registerColumnType(Types.BLOB, "bytea");
+    }
+
+    @Override
+    public SqlTypeDescriptor remapSqlTypeDescriptor(SqlTypeDescriptor sqlTypeDescriptor) {
+        if (sqlTypeDescriptor.getSqlType() == java.sql.Types.BLOB) {
+            return BinaryTypeDescriptor.INSTANCE;
+        }
+        return super.remapSqlTypeDescriptor(sqlTypeDescriptor);
+    }
+}
